141bc0595995579febfcaa123a7225b461b0f112
Kengo Yamaguchi <yamaguchikengo@gmail.com>
2017-10-11 08:22:12 +0000

    Change a Travis file

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -9,7 +9,7 @@ pip install --user rospkg
 #ros install
 cd ..
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
-cd ./ros_setup_scritps_Ubuntu14.04_server
+cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
 
